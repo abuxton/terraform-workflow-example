58f0c0dbdeade0f681a7001e749f68714aac45b4
Adam Buxton <buxton.adam@gmail.com>
2022-02-08 18:21:04 +0000

    do it

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-
-yello
+# Workflow test module
--- main.tf
+++ main.tf
@@ -2,18 +2,6 @@ resource "random_pet" "example" {
   prefix = var.random_prefix
 }
 
-resource "null_resource" "envs" {
-  provisioner "local-exec" {
-    command = "env| sort "
-  }
-}
-variable "tfc_workspace_slug"
- types=string
-}
-locals{
- org=regex("^[a-zA-Z0-9-]*",var.tfc_workspace_slug)
-}
-module "bootstrap" {
-  source  = "app.terraform.io/${org}/bootstrap/sentinel"
-  version = "0.1.2"
-}
+
+
+
