618be073093dd6214351bbacd27eca3572aef7dd
Adam Buxton <buxton.adam@gmail.com>
2022-02-10 08:37:19 +0000

    update remote

--- providers.tf
+++ providers.tf
@@ -1,12 +1,13 @@
 terraform {
   required_version = "~> 0.11.0"
 
-  backend "remote" {
+  remote {
     organization = "abc-hashi-training"
 
     workspaces {
-      name = "test-client"
+      name = "tf-workflow-example"
     }
+}
   }
 
   required_providers {
