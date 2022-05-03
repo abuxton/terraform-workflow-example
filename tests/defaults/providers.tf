be83dcfd9c1d886d9dd6abcfb76a9a41ab05512f
Adam Buxton <buxton.adam@gmail.com>
2022-05-03 14:26:20 +0100

    change provider

--- tests/defaults/providers.tf
+++ tests/defaults/providers.tf
@@ -1,17 +1,23 @@
 
 terraform {
+
+  cloud {
+    organization = "abc-vdm"
+
+    workspaces {
+      name = "workflow-example"
+    }
+  }
+
   #	required_version = "= 0.11.14"
   required_providers {
     random = {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
-    test = {
-      source = "terraform.io/builtin/test"
-    }
   }
 }
 
-provider "random" {
-  # Configuration options
-}
+// provider "random" {
+//   # Configuration options
+// }
