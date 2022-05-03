bc4a55bc7cd221c2fbce52de6eecc3934e405cb4
Adam Buxton <buxton.adam@gmail.com>
2022-05-03 14:48:10 +0100

    out put envs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "random-pet-name" {
   value = random_pet.example.id
 }
+output "tfx-env-map_tfe_run_id" {
+  # example from map
+  value = module.env.tfx-env-map.tfe_run_id
+}
+output "tfx-env-tfe_run_id" {
+  # example from module output
+  value = module.env.tfx-env-tfe_run_id
+}
