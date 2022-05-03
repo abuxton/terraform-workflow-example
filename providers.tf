ab97559de077ccee8f62902fe0daa1029ad6a7fe
Adam Buxton <buxton.adam@gmail.com>
2022-05-03 14:22:36 +0100

    update var

--- providers.tf
+++ providers.tf
@@ -1,5 +1,14 @@
 
 terraform {
+
+  cloud {
+    organization = "abc-vdm"
+
+    workspaces {
+      name = "workflow-example"
+    }
+  }
+
   #	required_version = "= 0.11.14"
   required_providers {
     random = {
